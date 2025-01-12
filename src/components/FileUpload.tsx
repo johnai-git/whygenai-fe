24cb9c30f857e77cbd875924905f593f8c48595c
imhidayatchitrali <hidayat.rehman64@gmail.com>
2025-01-12 21:40:51 +0500

    fixes

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -17,20 +17,20 @@ export function FileUpload({ bucket_name, onFileSelect }: FileUploadProps) {
       const formData = new FormData();
       formData.append('file', file);
       formData.append('bucket_name', bucket_name.toLocaleLowerCase()); // Add bucket name to the request
-      console.log({formData})
-      console.log({bucket_name})
-  
+      console.log({ formData })
+      console.log({ bucket_name })
+
       try {
         const response = await fetch('http://54.243.34.91:8000/upload/', {
-        // const response = await fetch('http://127.0.0.1:8000/upload/', {
+          // const response = await fetch('http://127.0.0.1:8000/upload/', {
           method: 'POST',
           body: formData, // Include the FormData with both file and bucket name
         });
-  
+
         if (!response.ok) {
           throw new Error('Failed to upload file');
         }
-  
+
         const data = await response.json();
         console.log('File upload successful:', data);
         alert(`File uploaded successfully: ${data.file_name}`);
@@ -40,19 +40,18 @@ export function FileUpload({ bucket_name, onFileSelect }: FileUploadProps) {
       } finally {
         setIsUploading(false);
       }
-  
+
       onFileSelect(bucket_name, file);
     }
   };
-  
+
 
   return (
     <div className="mt-4 border-t border-gray-100 pt-4">
       <label
         htmlFor={`file-upload-${bucket_name}`}
-        className={`flex items-center gap-2 text-sm font-medium ${
-          isUploading ? 'text-gray-400' : 'text-indigo-600 hover:text-indigo-700'
-        } cursor-pointer`}
+        className={`flex items-center gap-2 text-sm font-medium ${isUploading ? 'text-gray-400' : 'text-indigo-600 hover:text-indigo-700'
+          } cursor-pointer`}
       >
         <Upload className="w-4 h-4" />
         {isUploading ? 'Uploading...' : 'Upload Files'}
